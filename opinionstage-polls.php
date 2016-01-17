15ec8d7c79efd8404dd32e79d1ca7255d49d37d9
yoffegil <yoffegil@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-01-17 13:35:22 +0000

    Name fix
    git-svn-id: https://plugins.svn.wordpress.org/social-polls-by-opinionstage/trunk@1329965 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- opinionstage-polls.php
+++ opinionstage-polls.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Poll, Quiz & List Tools by OpinionStage
+Plugin Name: Poll, Quiz & List by OpinionStage
 Plugin URI: http://www.opinionstage.com
 Description: Adds highly engaging polls & quizzes to your site. Easily add polls & quizzes to any post/page or to your site sidebar.
 Version: 14.8.0
@@ -13,7 +13,7 @@ Text Domain: social-polls-by-opinionstage
 
 define('OPINIONSTAGE_SERVER_BASE', "www.opinionstage.com"); /* Don't include the protocol, added dynamically */
 define('OPINIONSTAGE_WIDGET_VERSION', '14.8.0');
-define('OPINIONSTAGE_WIDGET_PLUGIN_NAME', 'Poll, Quiz & List Tools by OpinionStage');
+define('OPINIONSTAGE_WIDGET_PLUGIN_NAME', 'Poll, Quiz & List by OpinionStage');
 define('OPINIONSTAGE_WIDGET_API_KEY', 'wp35e8');
 define('OPINIONSTAGE_OPTIONS_KEY', 'opinionstage_widget');
 define('OPINIONSTAGE_POLL_SHORTCODE', 'socialpoll');
