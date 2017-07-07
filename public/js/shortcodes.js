912ca33615c609c5911665909936c3564d2b31b0
kucaahbe <kucaahbe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2017-07-07 13:43:31 +0000

    remove redundancy in public/shortcode.js
    git-svn-id: https://plugins.svn.wordpress.org/social-polls-by-opinionstage/trunk@1692575 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- public/js/shortcodes.js
+++ public/js/shortcodes.js
@@ -1,15 +1,14 @@
-(function ($) {
-  $( document ).ready(function() {
-    $('[data-opinionstage-embed-url]').each(function( ) {
-      var $widgetPlacement = $(this);
-      var url = $widgetPlacement.data('opinionstage-embed-url');
+jQuery(function($) {
+  $('[data-opinionstage-embed-url]').each(function( ) {
+    var $widgetPlacement = $(this);
+    var url = $widgetPlacement.data('opinionstage-embed-url');
 
-      $.getJSON( url ).done( function( data ) {
+    $.getJSON( url ).
+      done( function( data ) {
         $widgetPlacement.after(data.code);
       })
       .fail(function(jqxhr, textStatus, error) {
         console.error( "[social-polls-by-opinionstage] can't load widget embed code" );
       });
-    });
   });
-})( jQuery );
+});
