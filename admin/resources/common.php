060ab61558ce7616ef81dc5b4ba3f67e1a6e95cb
kucaahbe <kucaahbe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2019-01-04 12:29:38 +0000

    Hotjar tracking
    git-svn-id: https://plugins.svn.wordpress.org/social-polls-by-opinionstage/trunk@2006277 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/resources/common.php
+++ admin/resources/common.php
@@ -17,7 +17,19 @@ function opinionstage_common_load_resources(){
 }
 
 function opinionstage_common_load_header(){
-
+	if(get_option('oswp_tracking_user_site_data') == 'yes'){ ?>
+		<!-- Hotjar Tracking Code for https://wordpress.org/plugins/social-polls-by-opinionstage/ -->
+		<script>
+		    (function(h,o,t,j,a,r){
+		        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+		        h._hjSettings={hjid:1142399,hjsv:6};
+		        a=o.getElementsByTagName('head')[0];
+		        r=o.createElement('script');r.async=1;
+		        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+		        a.appendChild(r);
+		    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+		</script>
+	<?php }
 }
 function opinionstage_common_load_footer(){
 
--- includes/opinionstage-ajax-functions.php
+++ includes/opinionstage-ajax-functions.php
@@ -6,6 +6,9 @@ defined( 'ABSPATH' ) or die();
 add_action( 'wp_ajax_opinionstage_ajax_toggle_flyout', 'opinionstage_ajax_toggle_flyout' );
 add_action( 'wp_ajax_opinionstage_ajax_toggle_article_placement', 'opinionstage_ajax_toggle_article_placement' );
 add_action( 'wp_ajax_opinionstage_ajax_toggle_sidebar_placement', 'opinionstage_ajax_toggle_sidebar_placement' );
+add_action( 'wp_ajax_opinionstage_ajax_tracking_user_data', 'opinionstage_ajax_tracking_user_data' );
+add_action( 'wp_ajax_osa_message_delete', 'opinionstage_message_delete' );
+
 
 // Toggle the flyout placement activation flag
 function opinionstage_ajax_toggle_flyout() {
@@ -29,6 +32,26 @@ function opinionstage_ajax_toggle_sidebar_placement() {
 	$os_options['sidebar_placement_active'] = $_POST['activate'];
 
 	update_option(OPINIONSTAGE_OPTIONS_KEY, $os_options);
+	wp_die('1');
+}
+
+function opinionstage_message_delete() {
+	if(isset($_POST['delete_options_oswp']) && $_POST['delete_options_oswp'] == true){
+		$message_title 		= delete_option('oswp_message_title');
+		$message_content 	= delete_option('oswp_message_content');
+		update_option('oswp_message_activity_time', time());
+	}
+	wp_die('1');
+}
+
+function opinionstage_ajax_tracking_user_data() {
+	if(isset($_POST['oswp_track_user_data']) && $_POST['oswp_track_user_data'] == 'yes'){
+		update_option('oswp_tracking_user_site_data', 'yes');
+	}elseif(isset($_POST['oswp_track_user_data']) && $_POST['oswp_track_user_data'] == 'no'){
+		update_option('oswp_tracking_user_site_data', 'no');
+	}else{
+
+	}
 	wp_die('1');
 }
 
