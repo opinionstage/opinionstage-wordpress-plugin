951fa73f11d6ef07cfd755210b889cfd96d7e7c4
kucaahbe <kucaahbe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2019-01-04 14:39:05 +0000

    version bump
    git-svn-id: https://plugins.svn.wordpress.org/social-polls-by-opinionstage/trunk@2006328 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Poll, Survey, Form & Quiz Maker by OpinionStage
 Plugin URI: https://www.opinionstage.com
 Description: Add a highly engaging poll, survey, quiz or contact form builder to your site. You can add the poll, survey, quiz or form to any post/page or to the sidebar.
-Version: 19.6.11
+Version: 19.6.12
 Author: OpinionStage.com
 Author URI: https://www.opinionstage.com
 Text Domain: social-polls-by-opinionstage
@@ -24,7 +24,7 @@ if ( defined('WP_DEBUG') && true === WP_DEBUG ) {
 	}
 }
 
-define('OPINIONSTAGE_WIDGET_VERSION', '19.6.11');
+define('OPINIONSTAGE_WIDGET_VERSION', '19.6.12');
 
 define('OPINIONSTAGE_TEXT_DOMAIN', 'social-polls-by-opinionstage');
 
