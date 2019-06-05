24381149743ef099b42a5d9359091c6ff7c66a6b
kucaahbe <kucaahbe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2019-06-05 08:43:12 +0000

    Support WP 5.2.x; Support for PHP v5.2 and above
    git-svn-id: https://plugins.svn.wordpress.org/social-polls-by-opinionstage/trunk@2100891 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -56,48 +56,83 @@ define('OPINIONSTAGE_VIEW_ITEM_SLUG', 'opinionstage-view-my-items');
 
 define('OPINIONSTAGE_LOGIN_CALLBACK_SLUG', 'opinionstage-login-callback');
 
-// Check if active plugin file is plugin.php on plugin activate hook
-function opinionstage_plugin_activated($plugin) {
-	if( $plugin == plugin_basename( __FILE__ ) ) {
-        exit( wp_redirect("admin.php?page=".OPINIONSTAGE_GETTING_STARTED_SLUG) );
-    }
-}
-function opinionstage_plugin_activate() {
-	// all good. delete old file
-	if( file_exists(__DIR__ . '/opinionstage-polls.php') ){
-		unlink(__DIR__ . '/opinionstage-polls.php');
-	}
-}
-register_activation_hook( __FILE__, 'opinionstage_plugin_activate' );
-add_action( 'init', 'opinionstage_plugin_activate' );
-add_action( 'activated_plugin', 'opinionstage_plugin_activated' );
-
-require_once( plugin_dir_path( __FILE__ ).'includes/opinionstage-functions.php' );
-
-// Check if another OpinionStage plugin already installed and display warning message.
-if (opinionstage_check_plugin_available('opinionstage_popup')) {
-	add_action('admin_notices', 'opinionstage_other_plugin_installed_warning');
+if ( ! version_compare( PHP_VERSION, '5.2', '>=' ) ) {
+	add_action( 'admin_notices', 'opinionstage_fail_php_version' );
+} elseif ( ! version_compare( get_bloginfo( 'version' ), '4.7', '>=' ) ) {
+	add_action( 'admin_notices', 'opinionstage_fail_wp_version' );
 } else {
-	require_once( plugin_dir_path( __FILE__ ).'includes/opinionstage-utility-functions.php' );
-	require_once( plugin_dir_path( __FILE__ ).'includes/opinionstage-article-placement-functions.php' );
-	require_once( plugin_dir_path( __FILE__ ).'includes/opinionstage-sidebar-widget.php' );
-
-	if ( (function_exists('wp_doing_ajax') && wp_doing_ajax()) || (defined('DOING_AJAX')) ) {
-		require_once( plugin_dir_path( __FILE__ ).'includes/opinionstage-ajax-functions.php' );
-		require( plugin_dir_path( __FILE__ ).'public/init.php' );
-	} else {
-		if ( is_admin() ) {
-			require( plugin_dir_path( __FILE__ ).'admin/init.php' );
-		} else {
-			require( plugin_dir_path( __FILE__ ).'public/init.php' );
+	// Check if active plugin file is plugin.php on plugin activate hook
+	function opinionstage_plugin_activated($plugin) {
+		if( $plugin == plugin_basename( __FILE__ ) ) {
+	        exit( wp_redirect("admin.php?page=".OPINIONSTAGE_GETTING_STARTED_SLUG) );
+	    }
+	}
+	function opinionstage_plugin_activate() {
+		// all good. delete old file
+		if( file_exists(plugin_dir_path( __FILE__ ) . '/opinionstage-polls.php') ){
+			unlink(plugin_dir_path( __FILE__ ) . '/opinionstage-polls.php');
 		}
 	}
-	// Block editor since 5.0.
-    $block_editor_oswp = version_compare( $GLOBALS['wp_version'], '5.0-beta', '>' );
-	if( function_exists( 'is_gutenberg_page' ) || $block_editor_oswp) {
-		require( plugin_dir_path( __FILE__ ).'gutenberg/init.php' );
+	register_activation_hook( __FILE__, 'opinionstage_plugin_activate' );
+	add_action( 'init', 'opinionstage_plugin_activate' );
+	add_action( 'activated_plugin', 'opinionstage_plugin_activated' );
+	require_once( plugin_dir_path( __FILE__ ).'includes/opinionstage-functions.php' );
+
+	// Check if another OpinionStage plugin already installed and display warning message.
+	if (opinionstage_check_plugin_available('opinionstage_popup')) {
+		add_action('admin_notices', 'opinionstage_other_plugin_installed_warning');
+	} else {
+		require_once( plugin_dir_path( __FILE__ ).'includes/opinionstage-utility-functions.php' );
+		require_once( plugin_dir_path( __FILE__ ).'includes/opinionstage-article-placement-functions.php' );
+		require_once( plugin_dir_path( __FILE__ ).'includes/opinionstage-sidebar-widget.php' );
+
+		if ( (function_exists('wp_doing_ajax') && wp_doing_ajax()) || (defined('DOING_AJAX')) ) {
+			require_once( plugin_dir_path( __FILE__ ).'includes/opinionstage-ajax-functions.php' );
+			require( plugin_dir_path( __FILE__ ).'public/init.php' );
+		} else {
+			if ( is_admin() ) {
+				require( plugin_dir_path( __FILE__ ).'admin/init.php' );
+			} else {
+				require( plugin_dir_path( __FILE__ ).'public/init.php' );
+			}
+		}
+		// Block editor since 5.0.
+	    $block_editor_oswp = version_compare( $GLOBALS['wp_version'], '5.0-beta', '>' );
+		if( function_exists( 'is_gutenberg_page' ) || $block_editor_oswp) {
+			require( plugin_dir_path( __FILE__ ).'gutenberg/init.php' );
+		}
+		add_action('widgets_init', 'opinionstage_init_widget');
+		add_action('plugins_loaded', 'opinionstage_init');
 	}
-	add_action('widgets_init', 'opinionstage_init_widget');
-	add_action('plugins_loaded', 'opinionstage_init');
 }
-?>
+/**
+ * Opinionstage admin notice for minimum PHP version.
+ *
+ * Warning when the site doesn't have the minimum required PHP version.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function opinionstage_fail_php_version() {
+	/* translators: %s: PHP version */
+	$message = sprintf( esc_html__( 'Poll, Survey, Form & Quiz Maker by OpinionStage requires PHP version %s+, plugin is currently NOT RUNNING.', 'opinionstage' ), '5.2' );
+	$html_message = sprintf( '<div class="error">%s</div>', wpautop( $message ) );
+	echo wp_kses_post( $html_message );
+}
+
+/**
+ * Opinionstage admin notice for minimum WordPress version.
+ *
+ * Warning when the site doesn't have the minimum required WordPress version.
+ *
+ * @since 1.5.0
+ *
+ * @return void
+ */
+function opinionstage_fail_wp_version() {
+	/* translators: %s: WordPress version */
+	$message = sprintf( esc_html__( 'Poll, Survey, Form & Quiz Maker by OpinionStage requires WordPress version %s+. Because you are using an earlier version, the plugin is currently NOT RUNNING.', 'opinionstage' ), '4.7' );
+	$html_message = sprintf( '<div class="error">%s</div>', wpautop( $message ) );
+	echo wp_kses_post( $html_message );
+}
