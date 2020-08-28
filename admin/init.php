43d3d4f288f18e702f01113f55a4480ef89efe66
kucaahbe <kucaahbe@ukr.net>
2020-08-28 17:32:57 +0300

    [techdebt] lint admin/init.php

--- admin/init.php
+++ admin/init.php
@@ -1,12 +1,15 @@
 <?php
-// block direct access to plugin PHP files:
-defined( 'ABSPATH' ) or die();
+/**
+ * Entry point for all plugin functionality for admin area
+ *
+ * @package   OpinionStageWordPressPlugin
+ */
 
-require( plugin_dir_path( __FILE__ ).'opinionstage-login-callback.php' );
-require( plugin_dir_path( __FILE__ ).'opinionstage-disconnect.php' );
-require( plugin_dir_path( __FILE__ ).'opinionstage-content-login-callback.php' );
-require( plugin_dir_path( __FILE__ ).'menu-page.php' );
-require( plugin_dir_path( __FILE__ ).'admin-page-loader.php' );
-require( plugin_dir_path( __FILE__ ).'message-handler.php' );
-	
-?>
+defined( 'ABSPATH' ) || die(); // block direct access to plugin PHP files.
+
+require plugin_dir_path( __FILE__ ) . 'opinionstage-login-callback.php';
+require plugin_dir_path( __FILE__ ) . 'opinionstage-disconnect.php';
+require plugin_dir_path( __FILE__ ) . 'opinionstage-content-login-callback.php';
+require plugin_dir_path( __FILE__ ) . 'menu-page.php';
+require plugin_dir_path( __FILE__ ) . 'admin-page-loader.php';
+require plugin_dir_path( __FILE__ ) . 'message-handler.php';
