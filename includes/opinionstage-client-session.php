4daef99c0c4d895e158eebbc4605e3a82eace9f7
kucaahbe <kucaahbe@ukr.net>
2020-09-29 13:18:48 +0300

    [lint] includes/opinionstage-client-session.php

--- includes/opinionstage-client-session.php
+++ includes/opinionstage-client-session.php
@@ -1,20 +1,34 @@
 <?php
-// block direct access to plugin PHP files:
-defined( 'ABSPATH' ) or die();
+/**
+ * User login-related functions, this is the only legitimate source of user logged in state.
+ *
+ * @package   OpinionStageWordPressPlugin
+ */
 
+defined( 'ABSPATH' ) || die(); // block direct access to plugin PHP files.
+
+/**
+ * Function, which answers to question "is user logged in?".
+ *
+ * @return bool True is user is logged in.
+ */
 function opinionstage_user_logged_in() {
-	$os_options = (array) get_option(OPINIONSTAGE_OPTIONS_KEY);
+	$os_options = (array) get_option( OPINIONSTAGE_OPTIONS_KEY );
 
-	return isset($os_options['uid']) && isset($os_options['email']);
+	return isset( $os_options['uid'] ) && isset( $os_options['email'] );
 }
 
+/**
+ * Get access to user token, in order to communicate to OpinionStage APIs.
+ *
+ * @return string|null User token or null if not found.
+ */
 function opinionstage_user_access_token() {
-	$os_options = (array) get_option(OPINIONSTAGE_OPTIONS_KEY);
+	$os_options = (array) get_option( OPINIONSTAGE_OPTIONS_KEY );
 
-	if ( isset($os_options['token']) ) {
+	if ( isset( $os_options['token'] ) ) {
 		return $os_options['token'];
 	} else {
 		return null;
 	}
 }
-?>
