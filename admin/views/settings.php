830bd212f04b2a52fae96fb3cefbafdc13c6d0ca
Artem <lapkin.artem@gmail.com>
2021-11-29 09:57:16 +0200

    chore: update text on add-to-site popup

--- admin/views/settings.php
+++ admin/views/settings.php
@@ -21,12 +21,11 @@ defined( 'ABSPATH' ) || die();
 			<div class="inner">
 				<span id="opinionstage-dialog-close" class="opinionstage-close"></span>
 				<div id="published-item-details">
-					<p><?php esc_html_e( 'You can add the item in the following ways:', 'social-polls-by-opinionstage' ); ?></p>
 					<ul>
-						<li><a href="https://help.opinionstage.com/en/articles/5161692-how-to-add-items-to-a-post-page" target="_blank"><?php esc_html_e( 'Add to a post/page', 'social-polls-by-opinionstage' ); ?></a></li>
-						<li><a href="https://help.opinionstage.com/en/articles/5161716-how-to-add-an-item-to-a-sidebar-widget" target="_blank"><?php esc_html_e( 'Add to a sidebar Widget', 'social-polls-by-opinionstage' ); ?></a></li>
-						<li><a href="https://help.opinionstage.com/en/articles/5161746-how-to-add-an-item-as-a-popup-on-wordpress" target="_blank"><?php esc_html_e( 'Add as a popup', 'social-polls-by-opinionstage' ); ?></a></li>
-						<li><a href="https://help.opinionstage.com/en/articles/5161782-how-to-add-an-item-using-the-wordpress-shortcode" target="_blank"><?php esc_html_e( 'Add with the WordPress shortcode', 'social-polls-by-opinionstage' ); ?></a></li>
+						<li><a href="https://help.opinionstage.com/en/articles/5161692-how-to-add-items-to-a-post-page" target="_blank"><?php esc_html_e( 'How to add to a post/page', 'social-polls-by-opinionstage' ); ?></a></li>
+						<li><a href="https://help.opinionstage.com/en/articles/5161716-how-to-add-an-item-to-a-sidebar-widget" target="_blank"><?php esc_html_e( 'How to add to a sidebar Widget', 'social-polls-by-opinionstage' ); ?></a></li>
+						<li><a href="https://help.opinionstage.com/en/articles/5161746-how-to-add-an-item-as-a-popup-on-wordpress" target="_blank"><?php esc_html_e( 'How to add as a popup', 'social-polls-by-opinionstage' ); ?></a></li>
+						<li><a href="https://help.opinionstage.com/en/articles/5161782-how-to-add-an-item-using-the-wordpress-shortcode" target="_blank"><?php esc_html_e( 'How to add with the WordPress shortcode', 'social-polls-by-opinionstage' ); ?></a></li>
 					</ul>
 					<div class="opinionstage-textarea-wrapper">
 						<textarea name="opinionstage-widget-shortcode" id="opinionstage-widget-shortcode" data-wp-embed-code rows="2" readonly="readonly"></textarea> <a data-copy-text-from="data-wp-embed-code" href="#" class="no-text-decoration">Copy</a>
